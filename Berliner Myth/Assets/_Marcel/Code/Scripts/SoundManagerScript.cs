1c4abb28c35f99e2bb0e20c79ba44eac41f5ab47
Mono_o <gamma.marcel@gmail.com>
2024-11-26 17:32:56 +0100

    added SoundManager
    arising FMOD issue

---
+++ Berliner Myth/Assets/_Marcel/Code/Scripts/SoundManagerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript: MonoBehaviour
+{
+    /*FMODUnity.StudioEventEmitter eventEmitterRef;
+
+    private FMOD.Studio.EventInstance PlaceHolder;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        eventEmitterRef = GetComponent<FMODUnity.StudioEventEmitter>();
+    }
+
+	private void Start()
+	{
+        PlaceHolder = FMODUnity.RuntimeManager.CreateInstance("event:/Test/PlaceHolder1");
+        PlaceHolder.start();
+    }
+
+
+	// Update is called once per frame
+	void Update()
+    {
+    }
+
+    void changeSoundVolume()
+	{
+
+	}*/
+}
+
+
+
+
