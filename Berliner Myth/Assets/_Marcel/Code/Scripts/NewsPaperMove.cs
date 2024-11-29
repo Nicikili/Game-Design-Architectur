b5ed68f0a3b0bc48736167373aa7517ea9fda7d3
Mono_o <gamma.marcel@gmail.com>
2024-11-29 17:08:30 +0100

    NewspaperAnimation for Ending done
    (still looks a bit ugly, but will try to fix another day)

--- Berliner Myth/Assets/_Marcel/Code/Scripts/NewsPaperMove.cs
+++ Berliner Myth/Assets/_Marcel/Code/Scripts/NewsPaperMove.cs
@@ -5,9 +5,13 @@ using DG.Tweening;
 
 public class NewsPaperMove : MonoBehaviour
 {
-    float endPos = 0;
+    float endPos = 5.5f;
+    float endPosReached;
     float endPosTime = 1f;
     bool snapping;
+
+    [SerializeField] GameObject Newspaper;
+    [SerializeField] GameObject NewspaperSlapped;
 
     [SerializeField] private AnimationCurve newsPaperAnimationCurve;
     // Start is called before the first frame update
@@ -25,9 +29,18 @@ public class NewsPaperMove : MonoBehaviour
     void NewspaperRotate()
 	{
 
-        transform.DORotate(new Vector3(0, 360, 0), 1f, RotateMode.FastBeyond360).SetRelative(true)
+        transform.DORotate(new Vector3(0, 360, 0), 0.5f, RotateMode.FastBeyond360).SetRelative(true)
             .SetEase(Ease.Linear).SetLoops(-1);
 
         transform.DOMoveY(endPos, endPosTime, snapping).SetEase(newsPaperAnimationCurve);
+
+        Debug.Log(transform.position.y);
+
+       if (transform.position.y > 5)
+        {
+           NewspaperSlapped.SetActive(true);
+           Newspaper.SetActive(false);
+           DOTween.Kill(transform);
+        }
     }
 }
