3e40d525bdb5735dc479b571b23219aa428e9bb1
Mono_o <gamma.marcel@gmail.com>
2024-12-14 15:14:11 +0100

    quick fix
    EndingScenes

--- Berliner Myth/Assets/_Marcel/Code/Scripts/NewsPaperMove.cs
+++ Berliner Myth/Assets/_Marcel/Code/Scripts/NewsPaperMove.cs
@@ -6,55 +6,54 @@ using UnityEngine.SceneManagement;
 
 public class NewsPaperMove : MonoBehaviour
 {
+	#region GameObjects and Variables
+    [SerializeField] GameObject Newspaper;
+    [SerializeField] GameObject NewspaperSlapped;
+    [SerializeField] GameObject Picture;
+
+    [SerializeField] private AnimationCurve newsPaperAnimationCurve;
+
     float endPos = 5.5f;
     float endPosTime = 1f;
     bool snapping;
 
     float endPosPicture = 13f;
     float endPosTimePicture = 45f;
+    #endregion
 
-    [SerializeField] GameObject Newspaper;
-    [SerializeField] GameObject NewspaperSlapped;
-    [SerializeField] GameObject Picture;
-
-    [SerializeField] private AnimationCurve newsPaperAnimationCurve;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame.
     void Update()
     {
         NewspaperRotate();
     }
 
-    void NewspaperRotate()
+    //code probably has to be made more efficient later down the road, 
+    //but currently works fine for the small task it needs to fulfill.
+	#region NewspaperMovement
+	void NewspaperRotate()
 	{
-
         transform.DORotate(new Vector3(0, 360, 0), 0.5f, RotateMode.FastBeyond360).SetRelative(true) //rotates the newspaper.
             .SetEase(Ease.Linear).SetLoops(-1);
 
         transform.DOMoveY(endPos, endPosTime, snapping).SetEase(newsPaperAnimationCurve); //moves the newspaper to front.
 
-        Debug.Log(transform.position.y);
-
        if (transform.position.y > 0) //if the newspaper reached a certain point, it will switch out with the new newspapperSlapped GameObject.
-        {
+       {
            NewspaperSlapped.SetActive(true);
            Newspaper.SetActive(false);
            DOTween.Kill(transform);
 
-           if (SceneManager.GetActiveScene().name == "EndingScene3_blue")
-           {
+			//move Newspaper Picture of Ending 3 (blue) down
+		    if (SceneManager.GetActiveScene().name == "EndingScene3_blue")
+            {
                 Picture.transform.DOLocalMoveY(endPosPicture, endPosTimePicture, snapping).SetEase(newsPaperAnimationCurve);
             }
 
-           if (SceneManager.GetActiveScene().name == "EndingScene3_red")
-           {
+            //move Newspaper Picture of Ending 3 (red) down
+            if (SceneManager.GetActiveScene().name == "EndingScene3_red")
+            {
                 Picture.transform.DOLocalMoveY(endPosPicture, endPosTimePicture, snapping).SetEase(newsPaperAnimationCurve);
-           }
-        }
+            }
+       }
     }
+    #endregion
 }
