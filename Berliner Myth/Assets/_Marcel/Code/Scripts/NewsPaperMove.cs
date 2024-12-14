cd0e2aec811f308e485baafd4161d607b55d1f73
Mono_o <gamma.marcel@gmail.com>
2024-12-14 14:51:25 +0100

    EndingScenes done
    ready to be connected

--- Berliner Myth/Assets/_Marcel/Code/Scripts/NewsPaperMove.cs
+++ Berliner Myth/Assets/_Marcel/Code/Scripts/NewsPaperMove.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class NewsPaperMove : MonoBehaviour
 {
     float endPos = 5.5f;
-    float endPosReached;
     float endPosTime = 1f;
     bool snapping;
 
+    float endPosPicture = 13f;
+    float endPosTimePicture = 45f;
+
     [SerializeField] GameObject Newspaper;
     [SerializeField] GameObject NewspaperSlapped;
+    [SerializeField] GameObject Picture;
 
     [SerializeField] private AnimationCurve newsPaperAnimationCurve;
     // Start is called before the first frame update
@@ -36,11 +40,21 @@ public class NewsPaperMove : MonoBehaviour
 
         Debug.Log(transform.position.y);
 
-       if (transform.position.y > 5) //if the newspaper reached a certain point, it will switch out with the new newspapperSlapped GameObject.
+       if (transform.position.y > 0) //if the newspaper reached a certain point, it will switch out with the new newspapperSlapped GameObject.
         {
            NewspaperSlapped.SetActive(true);
            Newspaper.SetActive(false);
            DOTween.Kill(transform);
+
+           if (SceneManager.GetActiveScene().name == "EndingScene3_blue")
+           {
+                Picture.transform.DOLocalMoveY(endPosPicture, endPosTimePicture, snapping).SetEase(newsPaperAnimationCurve);
+            }
+
+           if (SceneManager.GetActiveScene().name == "EndingScene3_red")
+           {
+                Picture.transform.DOLocalMoveY(endPosPicture, endPosTimePicture, snapping).SetEase(newsPaperAnimationCurve);
+           }
         }
     }
 }
