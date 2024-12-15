db36668e415846ca2367dffe9599581235b5b98e
Mono_o <gamma.marcel@gmail.com>
2024-12-15 18:37:39 +0100

    added music
    next: adding of more sound

--- Berliner Myth/Assets/_Marcel/Code/Scripts/AudioScripts/AudioManager.cs
+++ Berliner Myth/Assets/_Marcel/Code/Scripts/AudioScripts/AudioManager.cs
@@ -2,23 +2,155 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
+using UnityEngine.SceneManagement;
 //https://www.youtube.com/watch?v=rcBHIOjZDpk&ab_channel=ShapedbyRainStudios
 
 public class AudioManager : MonoBehaviour
 {
-	public static AudioManager instance { get; private set; }
+    [Header("Volume")]
+    [Range(0, 1)]
+    public float masterVolume = 1;
+    [Range(0, 1)]
+    public float musicVolume = 1;
+    [Range(0, 1)]
+    public float ambienceVolume = 1;
+    [Range(0, 1)]
+    public float SFXVolume = 1;
 
-	private void Awake()
-	{
-		if (instance != null)
-		{
-			Debug.LogError("Found more than one Audio Manager in the scene.");
-		}
-		instance = this;
-	}
+    private Bus masterBus;
+    private Bus musicBus;
+    private Bus ambienceBus;
+    private Bus sfxBus;
 
-	public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    private List<EventInstance> eventInstances;
+    private List<StudioEventEmitter> eventEmitters;
+
+    private EventInstance ambienceEventInstance;
+    private EventInstance musicEventInstance;
+
+    public static AudioManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Audio Manager in the scene.");
+        }
+        instance = this;
+
+        eventInstances = new List<EventInstance>();
+        eventEmitters = new List<StudioEventEmitter>();
+
+        //masterBus = RuntimeManager.GetBus("bus:/");
+        //musicBus = RuntimeManager.GetBus("bus:/Music");
+        //ambienceBus = RuntimeManager.GetBus("bus:/Ambience");
+        //sfxBus = RuntimeManager.GetBus("bus:/SFX");
+    }
+
+    private void Start()
+    {
+        InitializeAmbience(FMODEvents.instance.BackgroundNoise);
+        MusicForThisScene();
+    }
+
+    private void Update()
+    {
+        //masterBus.setVolume(masterVolume);
+        //musicBus.setVolume(musicVolume);
+        //ambienceBus.setVolume(ambienceVolume);
+        //sfxBus.setVolume(SFXVolume);
+    }
+
+    private void InitializeAmbience(EventReference ambienceEventReference)
+    {
+        ambienceEventInstance = CreateInstance(ambienceEventReference);
+        ambienceEventInstance.start();
+    }
+
+    private void InitializeMusic(EventReference musicEventReference)
+    {
+        musicEventInstance = CreateInstance(musicEventReference);
+        musicEventInstance.start();
+    }
+
+    public void SetAmbienceParameter(string parameterName, float parameterValue)
+    {
+        ambienceEventInstance.setParameterByName(parameterName, parameterValue);
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+
+    public EventInstance CreateInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(eventInstance);
+        return eventInstance;
+    }
+
+    public StudioEventEmitter InitializeEventEmitter(EventReference eventReference, GameObject emitterGameObject)
+    {
+        StudioEventEmitter emitter = emitterGameObject.GetComponent<StudioEventEmitter>();
+        emitter.EventReference = eventReference;
+        eventEmitters.Add(emitter);
+        return emitter;
+    }
+
+    private void CleanUp()
+    {
+        // stop and release any created instances
+        foreach (EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+        // stop all of the event emitters, because if we don't they may hang around in other scenes
+        foreach (StudioEventEmitter emitter in eventEmitters)
+        {
+            emitter.Stop();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CleanUp();
+    }
+
+	#region music
+	private void MusicForThisScene()
 	{
-	RuntimeManager.PlayOneShot(sound, worldPos);
-	}
+        if (SceneManager.GetActiveScene().name == "TitleScene")
+        {
+            InitializeMusic(FMODEvents.instance.ST_TitleScreen);
+        }
+
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            InitializeMusic(FMODEvents.instance.ST_InGame_Calm);
+        }
+        
+        if (SceneManager.GetActiveScene().name == "EndingScene1")
+        {
+            InitializeMusic(FMODEvents.instance.ST_Ending1);
+        }
+
+        if (SceneManager.GetActiveScene().name == "EndingScene2")
+        {
+            InitializeMusic(FMODEvents.instance.ST_Ending2);
+        }
+
+        if (SceneManager.GetActiveScene().name == "EndingScene3_blue")
+        {
+            InitializeMusic(FMODEvents.instance.ST_Ending3);
+        }
+
+        if (SceneManager.GetActiveScene().name == "EndingScene3_red")
+        {
+            InitializeMusic(FMODEvents.instance.ST_Ending3);
+        }
+    }
+    #endregion
 }
--- Berliner Myth/Assets/_Marcel/Code/Scripts/AudioScripts/FMODEvents.cs
+++ Berliner Myth/Assets/_Marcel/Code/Scripts/AudioScripts/FMODEvents.cs
@@ -2,11 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
 
 public class FMODEvents : MonoBehaviour
 {
-	[field: Header("SE_SwitchAround")]
+	[field: Header("UI")]
 	[field: SerializeField] public EventReference SE_SwitchAround { get; private set; }
+
+	#region ambiente
+	[field: Header("ambiente")]
+	[field: SerializeField] public EventReference BackgroundNoise { get; private set; }
+	#endregion
+
+	#region music
+	[field: Header("music")]
+	[field: SerializeField] public EventReference ST_TitleScreen { get; private set; }
+
+	[field: Header("music")]
+	[field: SerializeField] public EventReference ST_InGame_Calm { get; private set; }
+
+	[field: Header("music")]
+	[field: SerializeField] public EventReference ST_Ending1 { get; private set; }
+
+	[field: Header("music")]
+	[field: SerializeField] public EventReference ST_Ending2 { get; private set; }
+
+	[field: Header("music")]
+	[field: SerializeField] public EventReference ST_Ending3 { get; private set; }
+	#endregion
+
 	public static FMODEvents instance { get; private set; }
 	private void Awake()
 	{
